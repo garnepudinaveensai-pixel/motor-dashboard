cf88df26b1514061054a79537231bb00eb8aaadd
G Naveen Sai <garnepudinaveensai@gmail.com>
2026-02-24 15:51:30 +0530

    Made dashboard responsive

--- src/app/components/Gauge.js
+++ src/app/components/Gauge.js
@@ -175,7 +175,7 @@ export default function Gauge({
       </div>
 
       <div
-        style={{ marginTop: "-60px" }}
+        style={{ marginTop: "-45px" }}
         className="text-sm text-gray-400 tracking-wide"
       >
         {label}
